e330d91bff7fa8fb855d30e0f48b65ded3cf9a6b
Ksenia Shlyonskaya <xeniya.shlyonskaya@gmail.com>
2021-08-14 13:16:44 +0300

    initial project version

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="navbar">
+    <div @click="$router.push('/')">Vue 3</div>
+    <div class="navbar__btns">
+      <my-button @click="$router.push('/posts')">Посты</my-button>
+      <my-button @click="$router.push('/about')" style="margin-left: 20px">О проекте</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.navbar {
+  height: 50px;
+  background-color: lightcyan;
+  box-shadow: 2px 2px 4px gray;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+}
+
+.navbar__btns {
+  margin-left: auto;
+}
+</style>
---
+++ src/pages/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <h4>Данное приложение было создано в рамках обучения</h4>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+</style>
